4abc5c99e0ea023b4cabad1fda7d4ffd851f12a5
swanaik2018 <swanaik2018@gmail.com>
2025-08-28 09:23:12 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -4,6 +4,9 @@ from string_calculator import add
 class StringCalculatorTest(unittest.TestCase):
     def test_empty_string_returns_zero(self):
         self.assertEqual(add(""), 0)
+    def test_single_number_returns_its_value(self):
+        self.assertEqual(add("1"), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
